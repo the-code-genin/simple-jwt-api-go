980ae4c9f3c35889c03f0a5d2a6ddbf1b8fe584d
mohammed.adekunle <adekunle3317@gmail.com>
2023-03-16 01:33:46 +0100

    Implementd auth middleware

---
+++ api/middlewares.go
@@ -0,0 +1,77 @@
+package api
+
+import (
+	"fmt"
+	"regexp"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/the-code-genin/simple-jwt-api-go/database"
+	"github.com/the-code-genin/simple-jwt-api-go/internal"
+)
+
+func NewAuthMiddleware(config *internal.Config, users *database.Users) gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		re, err := regexp.Compile(`^Bearer\s+([^\s]+)$`)
+		if err != nil {
+			SendServerError(ctx, err.Error())
+			ctx.Abort()
+			return
+		}
+
+		authHeader := ctx.GetHeader("Authorization")
+		match := re.FindStringSubmatch(authHeader)
+		if len(match) != 2 {
+			SendServerError(ctx, "invalid Authorization header")
+			ctx.Abort()
+			return
+		}
+
+		token, err := jwt.Parse(match[1], func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return config.GetJWTKey()
+		})
+		if err != nil {
+			SendServerError(ctx, err.Error())
+			ctx.Abort()
+			return
+		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok || !token.Valid {
+			SendServerError(ctx, "invalid Authorization header")
+			ctx.Abort()
+			return
+		}
+
+		userID, userIDOk := claims["user_id"].(int)
+		userEmail, userEmailOk := claims["user_email"].(string)
+		exp, expOk := claims["exp"].(int64)
+		if !userIDOk || !userEmailOk || !expOk {
+			SendServerError(ctx, "invalid Authorization header")
+			ctx.Abort()
+			return
+		}
+
+		user, err := users.GetOne(userID)
+		if err != nil || user == nil {
+			SendServerError(ctx, "invalid Authorization header")
+			ctx.Abort()
+			return
+		} else if user.Email != userEmail {
+			SendServerError(ctx, "invalid Authorization header")
+			ctx.Abort()
+			return
+		} else if time.Now().Before(time.Unix(exp, 0)) {
+			SendServerError(ctx, "expired Authorization header")
+			ctx.Abort()
+			return
+		}
+
+		ctx.Set("auth_user", user)
+		ctx.Next()
+	}
+}
