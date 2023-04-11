797e6d56faaa31bc8f9baaf0941472c66e25a1c0
Mohammed Adekunle <mohammeda@lemonade.finance>
2023-04-11 09:21:58 +0100

    Restructured postgres connection

--- internal/postgres.go
+++ internal/postgres.go
@@ -1,12 +1,10 @@
 package internal
 
 import (
-	"context"
-
-	"github.com/jackc/pgx/v5"
+	"database/sql"
 )
 
 // Connect to postgres server
-func ConnectToPostgres(config Config) (*pgx.Conn, error) {
-	return pgx.Connect(context.Background(), config.DB.URL)
+func ConnectToPostgres(config Config) (*sql.DB, error) {
+	return sql.Open("postgres", config.DB.URL)
 }
