7e07339111970b8c495cbb2e517b6de18e184173
MacBobby Chibuzor <83423188+theghostmac@users.noreply.github.com>
2023-05-17 05:52:34 +0100

    Updated logger.go with comments for easy understanding

--- common/logger/logger.go
+++ common/logger/logger.go
@@ -8,20 +8,24 @@ import (
 	"github.com/inconshreveable/log15"
 )
 
+// Logger represents a structured logger.
 type Logger struct {
 	ctx    context.Context
 	fields map[string]interface{}
 }
 
+// WithField adds a key-value pair to the logger's fields and returns a new Logger instance.
 func (logger *Logger) WithField(name string, value interface{}) *Logger {
 	logger.fields[name] = value
 	return logger
 }
 
+// WithError adds an "error" field with the given error value to the logger's fields and returns a new Logger instance.
 func (logger *Logger) WithError(err error) *Logger {
 	return logger.WithField("error", err)
 }
 
+// buildOutput constructs the log output string by appending all fields in a formatted manner.
 func (logger *Logger) buildOutput(message string) string {
 	output := make([]string, 0)
 	for name, value := range logger.fields {
@@ -30,16 +34,19 @@ func (logger *Logger) buildOutput(message string) string {
 	return fmt.Sprintf("%s %s", message, strings.Join(output, " "))
 }
 
+// Error logs an error message with the formatted output and additional fields.
 func (logger *Logger) Error(format string, args ...interface{}) {
 	message := fmt.Sprintf(format, args...)
 	log15.Error(logger.buildOutput(message))
 }
 
+// Info logs an informational message with the formatted output and additional fields.
 func (logger *Logger) Info(format string, args ...interface{}) {
 	message := fmt.Sprintf(format, args...)
 	log15.Info(logger.buildOutput(message))
 }
 
+// NewLogger creates a new Logger instance with the given context.
 func NewLogger(ctx context.Context) *Logger {
 	return &Logger{ctx, make(map[string]interface{})}
 }
